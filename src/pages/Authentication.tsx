ed14aeda3700fa049772634169b6fd5f84acb99d
Mateen <mateenabdullopato@gmail.com>
2024-04-12 23:43:39 +0000

    fixed

--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router'
 
-const Authentication = () => {
+const Authentication:React.FC = () => {
   return (
     <div>
         <Outlet />
